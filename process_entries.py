5b9b0862ca99b9b1547c26a869f8083214c3ff4d
Scott Eppler <seppler@presidio.com>
2023-09-12 19:00:43 -0500

    Add check for Stripe Checkout status

--- process_entries.py
+++ process_entries.py
@@ -1,12 +1,15 @@
 import os
 import ssl
 import json
+import stripe
 import smtplib
 import mysql.connector
 from glob import glob
 from time import sleep
 from email.message import EmailMessage
 from PIL import Image, ImageDraw, ImageFont
+
+stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 
 def connect_db():
@@ -279,6 +282,11 @@ if __name__ == "__main__":
     if not os.path.exists(processed_dir):
         os.makedirs(processed_dir)
 
+    # Ensure 'failed' dir exists
+    failed_dir = "/data/failed"
+    if not os.path.exists(failed_dir):
+        os.makedirs(failed_dir)
+
     # Ensure 'badges' dir exists
     badges_dir = "/data/badges"
     if not os.path.exists(badges_dir):
@@ -295,12 +303,21 @@ if __name__ == "__main__":
             # Read JSON
             with open(json_file, "r") as f:
                 data = json.load(f)
+
+            checkout = stripe.checkout.Session.retrieve(data["checkout"])
+            if checkout.status == "open":
+                print("Waiting for Stripe Checkout")
+            elif checkout.status == "complete":
                 entry = write_entry(db_obj, data)
                 print(f"Entry #{entry} - {data['fname']} {data['lname']} added")
                 generate_badge(db_obj, entry)
                 send_email(db_obj, entry)
-            new_path = f"{processed_dir}/{os.path.basename(json_file)}"
-            os.rename(json_file, new_path)
+                new_path = f"{processed_dir}/{os.path.basename(json_file)}"
+                os.rename(json_file, new_path)
+            else:
+                print(f"Error processing {data['fname']} {data['lname']}")
+                new_path = f"{failed_dir}/{os.path.basename(json_file)}"
+                os.rename(json_file, new_path)
     else:
         print("Currently no entries to process. Waiting...")
 
