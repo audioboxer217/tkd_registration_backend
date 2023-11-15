054f2f424c346f078cc926275eba6e512313c1ed
Scott Eppler <seppler@presidio.com>
2023-11-14 19:50:58 -0600

    Update Badge Size

--- process_entries.py
+++ process_entries.py
@@ -83,46 +83,72 @@ def send_email(data):
 def generate_badge(data):
     """Generate an ID Badge using DB Data"""
 
+    # S3 Client
+    s3 = boto3.client("s3")
+
     # Opening the template image as the main badge
-    badge = Image.open(r"img/id_template.png")
+    # badge = Image.open(r"img/id_template.png")
+    badge = Image.new('RGBA', (400, 600), color='white')
     # Opening and resizing the profile image
     profile_img_string = s3.get_object(
-        Bucket=profile_pic_bucket,
+        Bucket=os.getenv("PROFILE_PIC_BUCKET"),
         Key=data["imgFilename"]["S"],
     )["Body"].read()
     profile_img = Image.open(io.BytesIO(profile_img_string))
-    profile_img = profile_img.resize((590, 585))
+    profile_img = profile_img.resize((250, 250))
 
     # Place profile image on background
-    badge.paste(profile_img, (301, 123))
+    badge.paste(profile_img, (75, 20))
 
     # Add text items
-    font = ImageFont.truetype("img/OpenSans-Regular.ttf", size=65)
+    font_name = ImageFont.truetype("img/OpenSans-Regular.ttf", size=30)
+    font = ImageFont.truetype("img/OpenSans-Regular.ttf", size=24)
     badge_draw = ImageDraw.Draw(badge)
-    # Ring Number
-    badge_draw.text((150, 175), "1", font=font, fill="white")
     # ID Number
-    badge_draw.text((1000, 175), "3", font=font, fill="white")
+    # badge_draw.text((37.5, 20), "ID", font=font, fill="black", anchor="ma")
+    # badge_draw.text((37.5, 50), "3", font=font, fill="black", anchor="ma")
+    # Ring Number
+    # badge_draw.text((362.5, 20), "Ring", font=font, fill="black", anchor="ma")
+    # badge_draw.text((362.5, 50), "__", font=font, fill="black", anchor="ma")
+    # Name
+    badge_draw.text((200, 275), data["full_name"]["S"], font=font_name, fill="black", anchor="mt")
+    # School
+    badge_draw.text((200, 310), data["school"]["S"], font=font, fill="black", anchor="ma")
     # Gender
-    badge_draw.text((190, 960), data["gender"]["S"], font=font, fill="black")
+    badge_draw.text((50, 350), f'Sex: {data["gender"]["S"]}', font=font, fill="black")
     # Age
-    badge_draw.text((190, 1050), data["age"]["N"], font=font, fill="black")
+    badge_draw.text((50, 380), f'Age: {data["age"]["N"]}', font=font, fill="black")
     # Belt
-    badge_draw.text((925, 960), data["beltRank"]["S"], font=font, fill="black")
+    badge_draw.text((235, 350), f'Belt: {data["beltRank"]["S"]}', font=font, fill="black")
     # Weight
-    badge_draw.text((925, 1050), f"{data['weight']['N']} kg", font=font, fill="black")
+    badge_draw.text((200, 380), f'Weight: {data["weight"]["N"]} kg', font=font, fill="black")
+    # Divider
+    badge_draw.line([(0, 420), (600, 420)], fill="black")
     # Events
+    badge_draw.text((200, 430), "Events", font=font, fill="black", anchor="mt")
     events = data["events"]["S"].split(",")
-    y = 1300
+    left_y = 450
+    left_x = 25
+    right_y = 450
+    right_x = 175
     for event in events:
-        badge_draw.text((150, y), f"• {event}", font=font, fill="black")
-        y += 100
+        if event in ['sparring','breaking','poomsae']:
+            x = left_x
+            y = left_y
+            left_y += 30
+        if event in ['pair poomsae','team poomsae','family poomsae']:
+            x = right_x
+            y = right_y
+            right_y += 30
+          
+        badge_draw.text((x, y), f"• {event}", font=font, fill="black")
 
     try:
         # Save the image for email attachment
+        badge = badge.resize((250,400), resample=Image.LANCZOS)
         badge = badge.convert("RGB")
         badge_filename = f"{data['pk']['S']}_badge.jpg".replace(" ", "_")
-        badge.save(os.path.join("/tmp", badge_filename))
+        badge.save(f"/tmp/{badge_filename}")
 
         # Save the image to an in-memory file for S3 Upload
         badge_file = io.BytesIO()
@@ -130,7 +156,7 @@ def generate_badge(data):
         badge_file.seek(0)
 
         # Upload to S3
-        s3.upload_fileobj(badge_file, badge_bucket, badge_filename)
+        s3.upload_fileobj(badge_file, os.getenv("BADGE_BUCKET"), badge_filename)
 
     except Exception as e:
         print(f"{e = }")
