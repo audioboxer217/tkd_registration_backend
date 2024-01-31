dcc037bb13202fb584823f0d4d588c516f1f64f6
Scott Eppler <seppler@presidio.com>
2024-01-31 09:24:45 -0600

    Update Gdrive Sync to be stand-alone

--- sync_aws_gdrive.py
+++ sync_aws_gdrive.py
@@ -1,16 +1,72 @@
+#!/usr/bin/env python
+
 import io
 import os
 import boto3
+import json
 from dotenv import load_dotenv
-import process_entries as pe
+from google.oauth2 import service_account
+import googleapiclient.discovery
+from apiclient import discovery
+from apiclient import errors
+
+from apiclient.http import MediaFileUpload
+from apiclient.http import MediaIoBaseUpload
 
 load_dotenv("../backend.env")
-drive = pe.gdrive_auth()
+s3 = boto3.client("s3")
 
 
-def check_gdrive_files():
+def gdrive_auth():
+    creds_file = s3.get_object(
+        Bucket=os.getenv("CONFIG_BUCKET"),
+        Key="tkd-reg_service_account.json",
+    )["Body"].read()
+
+    scopes_list = [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.file",
+    ]
+
+    creds_json = json.loads(creds_file)
+    credentials = service_account.Credentials.from_service_account_info(
+        creds_json, scopes=scopes_list
+    )
+
+    service = discovery.build(
+        "drive", "v3", credentials=credentials, cache_discovery=False
+    )
+
+    return service
+
+
+def upload_to_gdrive(file_obj, folder_id, file_name, drive_service):
+    # media_body = MediaFileUpload(file_obj, mimetype="image/jpeg")
+    media_body = MediaIoBaseUpload(
+        file_obj, mimetype="image/jpeg", chunksize=1024 * 1024, resumable=True
+    )
+
+    body = {
+        "name": file_name,
+        "title": file_name,
+        "description": file_name,
+        "mimeType": "image/jpeg",
+        "parents": [folder_id],
+    }
+
+    # note that supportsAllDrives=True is required or else the file upload will fail
+    file = (
+        drive_service.files()
+        .create(supportsAllDrives=True, body=body, media_body=media_body)
+        .execute()
+    )
+
+    return file
+
+
+def check_gdrive_files(drive_service):
     results = (
-        drive.files()
+        drive_service.files()
         .list(
             q="'" + os.getenv("BADGE_GFOLDER") + "' in parents",
             pageSize=10,
@@ -23,7 +79,7 @@ def check_gdrive_files():
     return items
 
 
-def upload_badges(existing_badges):
+def upload_badges(existing_badges, drive_service):
     existing_badge_names = [f["name"] for f in existing_badges]
 
     bucket = os.environ.get("BADGE_BUCKET")
@@ -33,12 +89,12 @@ def upload_badges(existing_badges):
 
     for badge in badges:
         if badge["Key"] in existing_badge_names:
-            print(f"{badge['Key']} Already Exists")
+            print(f"Remove existing badge: {badge['Key']}")
             fileId = next(
                 item["id"] for item in existing_badges if item["name"] == badge["Key"]
             )
 
-            drive.files().delete(fileId=fileId).execute()
+            drive_service.files().delete(fileId=fileId).execute()
 
         file_name = badge["Key"]
         print(f"Syncing {file_name}")
@@ -48,18 +104,19 @@ def upload_badges(existing_badges):
         file_obj.seek(0)
 
         # Upload to GDrive
-        pe.upload_to_gdrive(
+        upload_to_gdrive(
             file_obj,
             os.getenv("BADGE_GFOLDER"),
             file_name,
-            drive,
+            drive_service,
         )
         print("  done")
 
 
 def main():
-    existing_badges = check_gdrive_files()
-    upload_badges(existing_badges)
+    drive_service = gdrive_auth()
+    existing_badges = check_gdrive_files(drive_service)
+    upload_badges(existing_badges, drive_service)
 
 
 if __name__ == "__main__":
