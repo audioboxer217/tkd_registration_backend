ff32febe6ad3ea438b872304b9ba106f05c8088e
Scott Eppler <audioboxer217@gmail.com>
2024-11-21 22:49:31 -0600

    Update process_entries.py

--- process_entries.py
+++ process_entries.py
@@ -14,11 +14,14 @@ def add_entry_to_db(data):
     dynamodb = boto3.client("dynamodb")
     table_name = os.getenv("DB_TABLE")
 
-    school = data["school"]["S"].replace(" ", "_")
-    full_name = data["full_name"]["S"].replace(" ", "_")
+    if "school" in data:
+        school = f'{data["school"]["S"].replace(" ", "_")}'
+        pk = f"{school}{data['reg_type']['S']}-{data['full_name']['S'].replace(' ', '_')}"
+    else:
+        pk = f'{data["reg_type"]["S"]}-{data["full_name"]["S"].replace(" ", "_")}'
     data.update(
         dict(
-            pk={"S": f"{school}-{data['reg_type']['S']}-{full_name}"},
+            pk={"S": pk},
         )
     )
     dynamodb.put_item(
@@ -48,40 +51,17 @@ def send_email(data):
 
     reg_details = f"""
         Name: {data["full_name"]["S"]}
-        Type: {data["reg_type"]["S"]}
+        Item: {data["reg_type"]["S"]}
         Email: {data["email"]["S"]}
         Phone: {data["phone"]["S"]}
-        School: {data["school"]["S"]}
     """
-    if data["reg_type"]["S"] == "competitor":
-        reg_details += f"""    Coach: {data["coach"]["S"]}
+    if data["reg_type"]["S"] == "seminar":
+        reg_details += f"""    
+        School: {data["school"]["S"]}
         Parent: {data["parent"]["S"]}
         Birthdate: {data["birthdate"]["S"]}
         Gender: {data["gender"]["S"]}
-        Weight: {data["weight"]["N"]}
-        Belt: {data["beltRank"]["S"]}
-        Events:"""
-
-        for e in data["events"]["S"].split(','):
-            if e.startswith('sparring'):
-                spar_dict = {
-                    'wc': 'world class ',
-                    'gr': 'grass roots '
-                }
-                spar_parts = e.split('-')
-                if len(spar_parts) > 1:
-                    spar_type = spar_dict[spar_parts[1]]
-                else:
-                    spar_type = ''
-                e = f"{spar_type}sparring"
-
-            if e.endswith('poomsae') and e != "freestyle poomsae":
-                form_lookup = f"{e.replace(' ','_')}_form"
-                form_name = data[form_lookup]["S"]
-                if form_name.isnumeric():
-                    form_name = f"Taegeuk {form_name} Jang"
-                e += f" (Form: {form_name})"
-            reg_details += f"\n          • {e.title()}"
+        Belt: {data["beltRank"]["S"]}"""
 
     body = f"""
     Dear {data['full_name']['S']},
@@ -112,111 +92,6 @@ def send_email(data):
         smtp.sendmail(email_sender, email_receiver, em.as_string())
     
     ret_msg = "Mail Sent!"
-
-    print(ret_msg)
-    return ret_msg
-
-
-def generate_badge(data):
-    """Generate an ID Badge using DB Data"""
-
-    # S3 Client
-    s3 = boto3.client("s3")
-
-    # Opening the template image as the main badge
-    # badge = Image.open(r"img/id_template.png")
-    badge = Image.new("RGBA", (400, 600), color="white")
-    # Opening and resizing the profile image
-    profile_img_string = s3.get_object(
-        Bucket=os.getenv("PROFILE_PIC_BUCKET"),
-        Key=data["imgFilename"]["S"],
-    )["Body"].read()
-    profile_img = Image.open(io.BytesIO(profile_img_string))
-    profile_img = profile_img.resize((250, 250))
-    profile_img = ImageOps.exif_transpose(profile_img)
-
-    # Place profile image on background
-    badge.paste(profile_img, (75, 20))
-
-    # Add text items
-    font_name = ImageFont.truetype("img/OpenSans-Regular.ttf", size=30)
-    font = ImageFont.truetype("img/OpenSans-Regular.ttf", size=24)
-    badge_draw = ImageDraw.Draw(badge)
-    # ID Number
-    # badge_draw.text((37.5, 20), "ID", font=font, fill="black", anchor="ma")
-    # badge_draw.text((37.5, 50), "3", font=font, fill="black", anchor="ma")
-    # Ring Number
-    # badge_draw.text((362.5, 20), "Ring", font=font, fill="black", anchor="ma")
-    # badge_draw.text((362.5, 50), "__", font=font, fill="black", anchor="ma")
-    # Name
-    badge_draw.text(
-        (200, 275), data["full_name"]["S"], font=font_name, fill="black", anchor="mt"
-    )
-    # School
-    badge_draw.text(
-        (200, 310), data["school"]["S"], font=font, fill="black", anchor="ma"
-    )
-    # Gender
-    badge_draw.text((50, 350), f'Sex: {data["gender"]["S"]}', font=font, fill="black")
-    # Age
-    badge_draw.text((50, 380), f'Age: {data["age"]["N"]}', font=font, fill="black")
-    # Belt
-    badge_draw.text(
-        (235, 350), f'Belt: {data["beltRank"]["S"]}', font=font, fill="black"
-    )
-    # Weight
-    badge_draw.text(
-        (200, 380), f'Weight: {data["weight"]["N"]} lbs', font=font, fill="black"
-    )
-    # Divider
-    badge_draw.line([(0, 420), (600, 420)], fill="black")
-    # Events
-    badge_draw.text((200, 430), "Events", font=font, fill="black", anchor="mt")
-    events = data["events"]["S"].split(",")
-    left_y = 450
-    left_x = 25
-    right_y = 450
-    right_x = 175
-    for event in events:
-        if event in [
-            "sparring",
-            "sparring-gr",
-            "sparring-wc",
-            "breaking",
-            "poomsae",
-            "Little Tiger Showcase",
-        ]:
-            x = left_x
-            y = left_y
-            left_y += 30
-        if event in ["pair poomsae", "team poomsae", "family poomsae"]:
-            x = right_x
-            y = right_y
-            right_y += 30
-
-        badge_draw.text((x, y), f"• {event}", font=font, fill="black")
-
-    try:
-        # Resize and convert to final size/type
-        badge = badge.resize((250, 400), resample=Image.LANCZOS)
-        badge = badge.convert("RGB")
-        badge_filename = f"{data['pk']['S']}_badge.jpg".replace(" ", "_")
-
-        # Save the image for email attachment
-        # badge.save(f"/tmp/{badge_filename}")
-
-        # Save the image to an in-memory file for S3 Upload
-        badge_file = io.BytesIO()
-        badge.save(badge_file, format="JPEG")
-        badge_file.seek(0)
-
-        # Upload to S3
-        s3.upload_fileobj(badge_file, os.getenv("BADGE_BUCKET"), badge_filename)
-
-    except Exception as e:
-        ret_msg = f"{e = }"
-
-    ret_msg = f"Badge '{badge_filename}' generated"
 
     print(ret_msg)
     return ret_msg
@@ -283,15 +158,14 @@ def main(response):
                 print("Waiting for Stripe Checkout")
                 raise ValueError("Checkout Not Complete")
             elif checkout_status == "complete":
-                if data["reg_type"]["S"] == "competitor" and data["checkout"]["S"] != "manual_entry":
+                if data["checkout"]["S"] != "manual_entry":
                     data["payment"] = {"S": checkout.payment_intent}
                 del data["checkout"]
                 add_entry_to_db(data)
-                # if data["reg_type"]["S"] == "competitor":
-                #     generate_badge(data)
                 send_email(data)
                 print(f"  {data['full_name']['S']} Processed Successfully")
 
+            if data["reg_type"]["S"] == "seminar":
                 check_school(data)
 
         sqs_batch_response["batchItemFailures"] = batch_item_failures
