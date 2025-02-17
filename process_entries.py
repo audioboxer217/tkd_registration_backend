859cb06a47816ab08aa99b41c82600f106f52c64
Scott Eppler <audioboxer217@gmail.com>
2025-02-17 09:11:29 -0600

    Add handling of T-Shirt entry if exists
    Also, temp disable Pillow since we don't use it.

--- process_entries.py
+++ process_entries.py
@@ -7,7 +7,7 @@ import stripe
 import smtplib
 from email.message import EmailMessage
 from email.utils import formataddr
-from PIL import Image, ImageDraw, ImageFont, ImageOps
+# from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 
 def add_entry_to_db(data):
@@ -60,9 +60,12 @@ def send_email(data):
         Gender: {data["gender"]["S"]}
         Weight: {data["weight"]["N"]}
         Belt: {data["beltRank"]["S"]}
+        {f"T-Shirt size: {data['tshirt']['S']}" if "tshirt" in data else ''}
         Events:"""
 
         for e in data["events"]["S"].split(','):
+            if e == "little_dragon":
+                e = "Little Dragon Obstacle Course"
             if e.startswith('sparring'):
                 spar_dict = {
                     'wc': 'world class ',
@@ -117,109 +120,109 @@ def send_email(data):
     return ret_msg
 
 
-def generate_badge(data):
-    """Generate an ID Badge using DB Data"""
+# def generate_badge(data):
+#     """Generate an ID Badge using DB Data"""
 
-    # S3 Client
-    s3 = boto3.client("s3")
+#     # S3 Client
+#     s3 = boto3.client("s3")
 
-    # Opening the template image as the main badge
-    # badge = Image.open(r"img/id_template.png")
-    badge = Image.new("RGBA", (400, 600), color="white")
-    # Opening and resizing the profile image
-    profile_img_string = s3.get_object(
-        Bucket=os.getenv("PROFILE_PIC_BUCKET"),
-        Key=data["imgFilename"]["S"],
-    )["Body"].read()
-    profile_img = Image.open(io.BytesIO(profile_img_string))
-    profile_img = profile_img.resize((250, 250))
-    profile_img = ImageOps.exif_transpose(profile_img)
+#     # Opening the template image as the main badge
+#     # badge = Image.open(r"img/id_template.png")
+#     badge = Image.new("RGBA", (400, 600), color="white")
+#     # Opening and resizing the profile image
+#     profile_img_string = s3.get_object(
+#         Bucket=os.getenv("PROFILE_PIC_BUCKET"),
+#         Key=data["imgFilename"]["S"],
+#     )["Body"].read()
+#     profile_img = Image.open(io.BytesIO(profile_img_string))
+#     profile_img = profile_img.resize((250, 250))
+#     profile_img = ImageOps.exif_transpose(profile_img)
 
-    # Place profile image on background
-    badge.paste(profile_img, (75, 20))
+#     # Place profile image on background
+#     badge.paste(profile_img, (75, 20))
 
-    # Add text items
-    font_name = ImageFont.truetype("img/OpenSans-Regular.ttf", size=30)
-    font = ImageFont.truetype("img/OpenSans-Regular.ttf", size=24)
-    badge_draw = ImageDraw.Draw(badge)
-    # ID Number
-    # badge_draw.text((37.5, 20), "ID", font=font, fill="black", anchor="ma")
-    # badge_draw.text((37.5, 50), "3", font=font, fill="black", anchor="ma")
-    # Ring Number
-    # badge_draw.text((362.5, 20), "Ring", font=font, fill="black", anchor="ma")
-    # badge_draw.text((362.5, 50), "__", font=font, fill="black", anchor="ma")
-    # Name
-    badge_draw.text(
-        (200, 275), data["full_name"]["S"], font=font_name, fill="black", anchor="mt"
-    )
-    # School
-    badge_draw.text(
-        (200, 310), data["school"]["S"], font=font, fill="black", anchor="ma"
-    )
-    # Gender
-    badge_draw.text((50, 350), f'Sex: {data["gender"]["S"]}', font=font, fill="black")
-    # Age
-    badge_draw.text((50, 380), f'Age: {data["age"]["N"]}', font=font, fill="black")
-    # Belt
-    badge_draw.text(
-        (235, 350), f'Belt: {data["beltRank"]["S"]}', font=font, fill="black"
-    )
-    # Weight
-    badge_draw.text(
-        (200, 380), f'Weight: {data["weight"]["N"]} lbs', font=font, fill="black"
-    )
-    # Divider
-    badge_draw.line([(0, 420), (600, 420)], fill="black")
-    # Events
-    badge_draw.text((200, 430), "Events", font=font, fill="black", anchor="mt")
-    events = data["events"]["S"].split(",")
-    left_y = 450
-    left_x = 25
-    right_y = 450
-    right_x = 175
-    for event in events:
-        if event in [
-            "sparring",
-            "sparring-gr",
-            "sparring-wc",
-            "breaking",
-            "poomsae",
-            "Little Tiger Showcase",
-        ]:
-            x = left_x
-            y = left_y
-            left_y += 30
-        if event in ["pair poomsae", "team poomsae", "family poomsae"]:
-            x = right_x
-            y = right_y
-            right_y += 30
+#     # Add text items
+#     font_name = ImageFont.truetype("img/OpenSans-Regular.ttf", size=30)
+#     font = ImageFont.truetype("img/OpenSans-Regular.ttf", size=24)
+#     badge_draw = ImageDraw.Draw(badge)
+#     # ID Number
+#     # badge_draw.text((37.5, 20), "ID", font=font, fill="black", anchor="ma")
+#     # badge_draw.text((37.5, 50), "3", font=font, fill="black", anchor="ma")
+#     # Ring Number
+#     # badge_draw.text((362.5, 20), "Ring", font=font, fill="black", anchor="ma")
+#     # badge_draw.text((362.5, 50), "__", font=font, fill="black", anchor="ma")
+#     # Name
+#     badge_draw.text(
+#         (200, 275), data["full_name"]["S"], font=font_name, fill="black", anchor="mt"
+#     )
+#     # School
+#     badge_draw.text(
+#         (200, 310), data["school"]["S"], font=font, fill="black", anchor="ma"
+#     )
+#     # Gender
+#     badge_draw.text((50, 350), f'Sex: {data["gender"]["S"]}', font=font, fill="black")
+#     # Age
+#     badge_draw.text((50, 380), f'Age: {data["age"]["N"]}', font=font, fill="black")
+#     # Belt
+#     badge_draw.text(
+#         (235, 350), f'Belt: {data["beltRank"]["S"]}', font=font, fill="black"
+#     )
+#     # Weight
+#     badge_draw.text(
+#         (200, 380), f'Weight: {data["weight"]["N"]} lbs', font=font, fill="black"
+#     )
+#     # Divider
+#     badge_draw.line([(0, 420), (600, 420)], fill="black")
+#     # Events
+#     badge_draw.text((200, 430), "Events", font=font, fill="black", anchor="mt")
+#     events = data["events"]["S"].split(",")
+#     left_y = 450
+#     left_x = 25
+#     right_y = 450
+#     right_x = 175
+#     for event in events:
+#         if event in [
+#             "sparring",
+#             "sparring-gr",
+#             "sparring-wc",
+#             "breaking",
+#             "poomsae",
+#             "little_dragon",
+#         ]:
+#             x = left_x
+#             y = left_y
+#             left_y += 30
+#         if event in ["pair poomsae", "team poomsae", "family poomsae"]:
+#             x = right_x
+#             y = right_y
+#             right_y += 30
 
-        badge_draw.text((x, y), f"• {event}", font=font, fill="black")
+#         badge_draw.text((x, y), f"• {event}", font=font, fill="black")
 
-    try:
-        # Resize and convert to final size/type
-        badge = badge.resize((250, 400), resample=Image.LANCZOS)
-        badge = badge.convert("RGB")
-        badge_filename = f"{data['pk']['S']}_badge.jpg".replace(" ", "_")
+#     try:
+#         # Resize and convert to final size/type
+#         badge = badge.resize((250, 400), resample=Image.LANCZOS)
+#         badge = badge.convert("RGB")
+#         badge_filename = f"{data['pk']['S']}_badge.jpg".replace(" ", "_")
 
-        # Save the image for email attachment
-        # badge.save(f"/tmp/{badge_filename}")
+#         # Save the image for email attachment
+#         # badge.save(f"/tmp/{badge_filename}")
 
-        # Save the image to an in-memory file for S3 Upload
-        badge_file = io.BytesIO()
-        badge.save(badge_file, format="JPEG")
-        badge_file.seek(0)
+#         # Save the image to an in-memory file for S3 Upload
+#         badge_file = io.BytesIO()
+#         badge.save(badge_file, format="JPEG")
+#         badge_file.seek(0)
 
-        # Upload to S3
-        s3.upload_fileobj(badge_file, os.getenv("BADGE_BUCKET"), badge_filename)
+#         # Upload to S3
+#         s3.upload_fileobj(badge_file, os.getenv("BADGE_BUCKET"), badge_filename)
 
-    except Exception as e:
-        ret_msg = f"{e = }"
+#     except Exception as e:
+#         ret_msg = f"{e = }"
 
-    ret_msg = f"Badge '{badge_filename}' generated"
+#     ret_msg = f"Badge '{badge_filename}' generated"
 
-    print(ret_msg)
-    return ret_msg
+#     print(ret_msg)
+#     return ret_msg
 
 
 def check_school(data):
